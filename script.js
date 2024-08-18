344714c560e57f47bc32b7c8164f43b6d4457079
Kirito72-hub <ahmed.saudi1996@gmail.com>
2024-08-18 11:25:54 +0300

    added template operator function empty for now

--- script.js
+++ script.js
@@ -16,3 +16,8 @@ function multiply(x, y){
 function divide(x, y){
     return x/y;
 }
+
+//empty function for now will take 2 numbers and 1 operator and according to the operator will call operator function from the above ones
+function operator(x, y, operator){
+    return;
+}
