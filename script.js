5ecf9d7cfdd70ce54614d7028cbbf93790ae27ac
Kirito72-hub <ahmed.saudi1996@gmail.com>
2024-08-18 11:20:25 +0300

    wrote the 4 basic functions (add, subtract, multiply, divide)

---
+++ script.js
@@ -0,0 +1,16 @@
+// adding 4 functions add subtract multiply divide
+function add(x, y){
+    return x*y;
+}
+
+function subtract(x, y){
+    return x-y;
+}
+
+function multiply(x, y){
+    return x*y;
+}
+
+function divide(x, y){
+    return x/y;
+}
