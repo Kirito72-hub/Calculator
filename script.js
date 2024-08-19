6e0ef37e975b81ea3b4eead972624191d5d643e0
Kirito72-hub <ahmed.saudi1996@gmail.com>
2024-08-19 07:40:42 +0300

    added function to update the screen

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//popluate screen input
+let displayValue = "";
 //General variables for now
 let x, y, operator;
 // adding 4 functions add subtract multiply divide
@@ -20,4 +22,11 @@ function divide(x, y){
 //empty function for now will take 2 numbers and 1 operator and according to the operator will call operator function from the above ones
 function operator(x, y, operator){
     return;
+}
+
+//function that update the display
+function updateDisplay(value){
+    const display = document.querySelectorAll(".screen");
+    displayValue += value;
+    display.textContent = displayValue;
 }
